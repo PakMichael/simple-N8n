960c90b9ab0c626f6e55347b6f073002f994759f
Misha Pak <mishapak@protonmail.com>
2026-05-16 20:50:43 +0200

    feat: Create Condition model connected to tasks

--- public_python/workflow/models.py
+++ public_python/workflow/models.py
@@ -1,6 +1,6 @@
 import uuid as _uuid
 
-from sqlalchemy import ForeignKey, Boolean
+from sqlalchemy import ForeignKey, Boolean, UniqueConstraint, String
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import Mapped, mapped_column
 
@@ -25,3 +25,37 @@ class FlowTask(Base):
         ForeignKey("task.uuid", ondelete="CASCADE"),
         unique=True,
     )
+
+class Condition(Base):
+    __tablename__ = "condition_task"
+
+    uuid: Mapped[_uuid.UUID] = mapped_column(
+        UUID(as_uuid=True), primary_key=True, default=_uuid.uuid4
+    )
+
+    name: Mapped[str] = mapped_column(String)
+    description: Mapped[str] = mapped_column(String)
+
+    flow_uuid: Mapped[_uuid.UUID] = mapped_column(
+        UUID(as_uuid=True),
+        ForeignKey("flow.uuid", ondelete="CASCADE"),
+    )
+
+    source_task_uuid: Mapped[_uuid.UUID] = mapped_column(
+        UUID(as_uuid=True),
+        ForeignKey("task.uuid", ondelete="CASCADE"),
+    )
+    target_task_success: Mapped[_uuid.UUID] = mapped_column(
+        UUID(as_uuid=True),
+        ForeignKey("task.uuid", ondelete="CASCADE"),
+    )
+    target_task_failure: Mapped[_uuid.UUID] = mapped_column(
+        UUID(as_uuid=True),
+        ForeignKey("task.uuid", ondelete="CASCADE"),
+        nullable=True,
+    )
+
+    __table_args__ = (
+        UniqueConstraint("source_task_uuid", "target_task_success", "target_task_failure"),
+    )
+
