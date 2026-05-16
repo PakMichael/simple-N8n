4751bc39edfe0f3715c498c8aee5859e361b7eff
Misha Pak <mishapak@protonmail.com>
2026-05-16 19:48:02 +0200

    feat: Create flow model

---
+++ public_python/flow/models.py
@@ -0,0 +1,16 @@
+import uuid as _uuid
+
+from sqlalchemy import String
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import Mapped, mapped_column
+
+from public_python.db_config import Base
+
+
+class Flow(Base):
+    __tablename__ = "flow"
+
+    uuid: Mapped[_uuid.UUID] = mapped_column(
+        UUID(as_uuid=True), primary_key=True, default=_uuid.uuid4
+    )
+    name: Mapped[str] = mapped_column(String)
