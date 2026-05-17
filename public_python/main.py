b28c67df409be30497cdeb68fe6493d9c9eb68e8
Misha Pak <mishapak@protonmail.com>
2026-05-17 19:59:41 +0200

    fix(main.py): Attach routers and enable constructor on startup

--- public_python/main.py
+++ public_python/main.py
@@ -1,3 +1,14 @@
+import asyncio
+
 from fastapi import FastAPI
 
+from public_python.consumers import main_loop_consumer
+from public_python.workflow.routes import workflow_router
+
 app = FastAPI()
+
+app.include_router(workflow_router, prefix="/api")
+
+@app.on_event("startup")
+async def start_consumer():
+    asyncio.create_task(main_loop_consumer())
