b3882032d7bdea1361fa2865220ed621bbf7f293
Misha Pak <mishapak@protonmail.com>
2026-05-17 20:35:02 +0200

    feat: Create /build-flow endpoint to load .json

--- public_python/workflow/domain/entities.py
+++ public_python/workflow/domain/entities.py
@@ -21,3 +21,29 @@ class CreateFlowCondition(BaseModel):
     target_task_success_uuid: uuid.UUID
     target_task_failure_uuid: uuid.UUID | None = None
     flow_uuid: uuid.UUID
+
+
+class RawTask(BaseModel):
+    name: str
+    description: str
+
+
+class RawCondition(BaseModel):
+    name: str
+    description: str
+    source_task: str
+    outcome: str  # ignored in the project - meaning unclear
+    target_task_success: str
+    target_task_failure: str
+
+
+class RawFlow(BaseModel):
+    id: str
+    name: str
+    start_task: str
+    tasks: list[RawTask]
+    conditions: list[RawCondition]
+
+
+class FlowBuilder(BaseModel):
+    flow: RawFlow
--- public_python/workflow/routes.py
+++ public_python/workflow/routes.py
@@ -8,11 +8,12 @@ from public_python.constants import MAIN_LOOP_CHANNEL
 from public_python.db_config import AsyncSessionDep
 from public_python.flow.models import Flow
 from public_python.redis_conf import async_redis_client
-from public_python.task.models import Task
+from public_python.task.models import Task, TaskType
 from public_python.workflow.domain.entities import (
     FlowCreate,
     CreateFlowTask,
     CreateFlowCondition,
+    FlowBuilder,
 )
 from public_python.workflow.models import FlowTask, Condition
 
@@ -97,3 +98,50 @@ async def flow_start(flow_uuid: uuid.UUID, session: AsyncSessionDep):
     )
 
     return "Flow started"
+
+
+@workflow_router.post("/build-flow/")
+async def build_flow(raw_flow: FlowBuilder, session: AsyncSessionDep):
+    task_uuids = {"end": None}  # Json provides 'end' value as to mean 'do not continue'
+
+    async with session.begin():
+        flow = FlowCreate(name=raw_flow.flow.name)
+
+        db_flow = Flow(name=flow.name)
+
+        session.add(db_flow)
+
+        await session.flush()
+
+        for raw_task in raw_flow.flow.tasks:
+            db_task = Task(
+                name=raw_task.name,
+                description=raw_task.description,
+                type=TaskType.fetch,
+            )
+
+            session.add(db_task)
+            await session.flush()
+
+            task_uuids.update({db_task.name: db_task.uuid})
+
+            db_flow_task = FlowTask(
+                flow_uuid=db_flow.uuid,
+                task_uuid=db_task.uuid,
+                is_start_task=raw_task.name == raw_flow.flow.start_task,
+            )
+            session.add(db_flow_task)
+
+        for raw_condition in raw_flow.flow.conditions:
+            db_condition = Condition(
+                name=raw_condition.name,
+                description=raw_condition.description,
+                flow_uuid=db_flow.uuid,
+                source_task_uuid=task_uuids[raw_condition.source_task],
+                target_task_success=task_uuids[raw_condition.target_task_success],
+                target_task_failure=task_uuids[raw_condition.target_task_failure],
+            )
+
+            session.add(db_condition)
+
+    return raw_flow
